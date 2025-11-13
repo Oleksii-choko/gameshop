1a1d9af4b67cf685c61368126189f3bcc5396e24
Choko <alexejlevochkin@gmail.com>
2025-11-13 15:49:31 +0200

    version 1.1.1 complete start configuration index, create base file

--- gameshop/views.py
+++ gameshop/views.py
@@ -5,3 +5,21 @@ from django.contrib.auth import login, logout
 from django.contrib import messages
 
 
+from .models import Category, Game
+
+class Index(ListView):
+    """Головна сторінка"""
+    model = Game
+    # context_object_name = 'categories'
+    extra_context = {'title': 'Главная страница'}
+    template_name = 'gameshop/index.html'
+
+    # def get_queryset(self):
+    #     """Вывод родительских категорий"""
+    #     return ...
+
+    # def get_context_data(self, *, object_list=None, **kwargs):
+    # """Вывод на страничку дополнительных элементов"""
+    # context = super().get_context_data()  # Словарь
+    # context['top_product'] = Product.objects.order_by('-watched')[:3]
+    # return context
